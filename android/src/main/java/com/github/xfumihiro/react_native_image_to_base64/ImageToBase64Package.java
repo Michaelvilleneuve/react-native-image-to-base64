433c3b2b3932485aec6cddbf6dcd902004e43aaa
Orhan Sönmez <orhnsnmz@googlemail.com>
2017-09-30 15:22:55 +0200

    Update ImageToBase64Package.java

--- android/src/main/java/com/github/xfumihiro/react_native_image_to_base64/ImageToBase64Package.java
+++ android/src/main/java/com/github/xfumihiro/react_native_image_to_base64/ImageToBase64Package.java
@@ -22,7 +22,7 @@ public class ImageToBase64Package implements ReactPackage {
     return modules;
   }
 
-  @Override
+  // Deprecated in RN0.47+
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
