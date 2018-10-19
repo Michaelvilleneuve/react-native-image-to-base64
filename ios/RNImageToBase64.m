0642eef8642fd7ab62c18d6cc8b44ac39e969cb9
Selma Zaim <s.zaim@myunisoft.fr>
2018-10-19 15:33:14 +0200

    iOS was not working anymore because of ALA deprecated library since iOS 9, I refacto all the iOS part

--- ios/RNImageToBase64.m
+++ ios/RNImageToBase64.m
@@ -1,4 +1,13 @@
+//
+//  UIImage+Orientation.m
+//  RNImageToBase64
+//
+//  Created by Selma on 19/10/2018.
+//  Copyright © 2018 xfumihiro. All rights reserved.
+//
+
 #import "RNImageToBase64.h"
+#import "UIImage+Orientation.h"
 #import <AssetsLibrary/AssetsLibrary.h>
 #import <UIKit/UIKit.h>
 
@@ -8,35 +17,23 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(getBase64String:(NSString *)input callback:(RCTResponseSenderBlock)callback)
 {
-  NSURL *url = [[NSURL alloc] initWithString:input];
-  ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-  [library assetForURL:url resultBlock:^(ALAsset *asset) {
-    CGImageRef imageRef = [self rotatePortraitImageFromAsset:asset];
-
-    NSData *imageData = UIImagePNGRepresentation([UIImage imageWithCGImage:imageRef]);
-    NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
-    callback(@[[NSNull null], base64Encoded]);
-
-  } failureBlock:^(NSError *error) {
-    NSLog(@"that didn't work %@", error);
-    callback(@[error]);
-  }];
-}
-
--(CGImageRef) rotatePortraitImageFromAsset:(ALAsset*) asset {
-    ALAssetRepresentation *rep = [asset defaultRepresentation];
-
-    if ([rep orientation] == ALAssetOrientationUp){
-        return [rep fullScreenImage];
-    } else {
-        CGImageRef ref = [rep fullScreenImage];
-        UIImage* testImage = [UIImage imageWithCGImage:ref];
-
-        UIGraphicsBeginImageContextWithOptions(testImage.size, NO, testImage.scale);
-        [testImage drawInRect:(CGRect){0, 0, testImage.size}];
-        UIImage *normalizedImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return [testImage CGImage];
+    @try {
+        NSURL *url = [NSURL fileURLWithPath:input];
+        NSError *err = nil;
+        NSData *imageData = [NSData dataWithContentsOfURL:url options: NSDataReadingUncached error: &err];
+        if (err != nil) {
+            callback(@[@"Not found image"]);
+            return;
+        }
+        UIImage *image = [UIImage imageWithData: imageData];
+        UIImage *portraitImage = [image normalizedImage];
+        NSData *imgData = UIImageJPEGRepresentation(portraitImage, 1.0);
+        NSString *base64 = [imgData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+        callback(@[[NSNull null], base64]);
+    }
+    @catch (NSException *exception) {
+        NSLog(@"%@", exception.reason);
+        callback(@[exception.reason]);
     }
 }
 
---
+++ ios/UIImage+Orientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Orientation.h
+//  RNImageToBase64
+//
+//  Created by Selma on 19/10/2018.
+//  Copyright © 2018 xfumihiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Orientation)
+
+- (UIImage *)normalizedImage;
+
+@end
